2fbeeb88f22dbbbc59bd05cff6b9cd966407413f
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-03-27 15:17:57 +0200

    Update setup

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import dotenv from 'dotenv'
+import { App } from '@slack/bolt'
 
 dotenv.config()
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+async () => {
+    app.start(process.env.PORT || 3000);
+
+    console.log('⚡️ Bolt app is running!');
+}
