13c49753e2cd3bb289179eac36d4ffb0f8b09792
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-04-11 14:49:40 +0200

    Wip

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { getChannelFromChannelName } from '../slack'
 
 const routes = Router()
 
@@ -6,6 +7,24 @@ routes.get('/isalive', async (req, res, next) => {
     res.sendStatus(200)
 })
 routes.get('/isready', async (req, res, next) => {
+    res.sendStatus(200)
+})
+
+routes.get('/validChannel/:channelName', async (req, res, next) => {
+    console.log(req.params.channelName)
+    const channel = await getChannelFromChannelName(req.params.channelName, '')
+
+    console.log(channel)
+    // try {
+    //     if (await getChannelFromChannelName(req.params.channelName, '')) {
+    //         res.sendStatus(200);
+    //     } else {
+    //         res.sendStatus(404);
+    //     }
+    // } catch(error) {
+    //     res.sendStatus(500)
+    // }
+
     res.sendStatus(200)
 })
 
--- src/slack/index.ts
+++ src/slack/index.ts
@@ -1,32 +1,27 @@
 import dotenv from 'dotenv'
 import { App } from '@slack/bolt'
+import { slackApp } from '../loaders'
+import config from '../config'
 
-dotenv.config()
-
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
-});
-
-async function getValidChannelID(channelName: string, cursor: string) {
-    const conversations = await app.client.conversations.list({ token: process.env.SLACK_BOT_TOKEN, limit: 200, cursor })
+export const getChannelFromChannelName = async (channelName: string, cursor: string) => {
+    const conversations = await slackApp.client.conversations.list({ token: config.slack.botToken, limit: 1000, cursor })
     
     const channel = conversations.channels?.find(obj => {
         return obj.name === channelName
     })
 
     if (channel !== undefined && !channel.is_archived) {
-        await app.client.chat.postMessage({
-            token: process.env.SLACK_BOT_TOKEN,
-            channel: channel.id!,
-            text: 'Ditt samtykke for: Brukertest av Los Pollos Hermanos. Har utløpt'
-        })
-        return
-    }
-
+        console.log(channel)
+        return channel
+    } 
+    
     if (conversations.response_metadata?.next_cursor !== '' && conversations.response_metadata?.next_cursor !== undefined) {
-        getValidChannelID(channelName, conversations.response_metadata?.next_cursor)
+        await getChannelFromChannelName(channelName, conversations.response_metadata?.next_cursor)
     }
 }
 
-getValidChannelID('samtykke-bot-test', '')
+// await app.client.chat.postMessage({
+//             token: process.env.SLACK_BOT_TOKEN,
+//             channel: channel.id!,
+//             text: 'Ditt samtykke for: Brukertest av Los Pollos Hermanos. Har utløpt'
+//         })
